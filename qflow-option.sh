209a34e724e68dd3800aa2cd3068d1d736b28de7
Mizuki <126767133+3zki@users.noreply.github.com>
2024-08-06 01:02:59 +0900

    Add qflow setup script

---
+++ qflow-option.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+# ========================================================================
+# QFlow options
+# ========================================================================
+
+# Define setup environment
+# ------------------------
+export PDK_ROOT="$HOME/pdk"
+export SRC_DIR="$HOME/src"
+my_path=$(realpath "$0")
+my_dir=$(dirname "$my_path")
+export SCRIPT_DIR="$my_dir"
+
+
+# Install/update qrouter
+# --------------------
+if [ ! -d "$SRC_DIR/qrouter" ]; then
+	echo ">>>> Installing qrouter"
+	git clone https://github.com/RTimothyEdwards/qrouter.git "$SRC_DIR/qrouter"
+	cd "$SRC_DIR/qrouter" || exit
+	./configure
+else
+	echo ">>>> Updating qrouter"
+	cd "$SRC_DIR/qrouter" || exit
+	git pull
+fi
+make && sudo make install
+make clean
+
+# Install/update graywolf
+# --------------------
+if [ ! -d "$SRC_DIR/graywolf" ]; then
+	echo ">>>> Installing graywolf"
+	sudo apt -qq install -y libgsl-dev
+	git clone https://github.com/RTimothyEdwards/graywolf.git "$SRC_DIR/graywolf"
+	cd "$SRC_DIR/graywolf" || exit
+	./configure
+else
+	echo ">>>> Updating graywolf"
+	cd "$SRC_DIR/graywolf" || exit
+	git pull
+fi
+mkdir build
+cd build
+cmake ..
+make  
+sudo make install  
+
+# Install/update yosys
+# --------------------
+if [ ! -d "$SRC_DIR/yosys" ]; then
+	echo ">>>> Installing yosys"
+	sudo apt -qq install -y clang bison flex \
+	libreadline-dev gawk tcl-dev libffi-dev git \
+	graphviz xdot pkg-config python3 libboost-system-dev \
+	libboost-python-dev libboost-filesystem-dev zlib1g-dev
+	git clone https://github.com/YosysHQ/yosys.git "$SRC_DIR/yosys"
+	cd "$SRC_DIR/yosys" || exit
+	./configure
+else
+	echo ">>>> Updating yosys"
+	cd "$SRC_DIR/yosys" || exit
+	git pull
+fi
+make config-clang
+make -j"$(nproc)" && sudo make install
+make clean
+
+# Install/update qflow
+# --------------------
+if [ ! -d "$SRC_DIR/qflow" ]; then
+	echo ">>>> Installing qflow"
+	sudio apt -qq install -y python-tk
+	git clone https://github.com/RTimothyEdwards/qflow.git "$SRC_DIR/qflow"
+	cd "$SRC_DIR/qflow" || exit
+	./configure
+else
+	echo ">>>> Updating yosys"
+	cd "$SRC_DIR/qflow" || exit
+	git pull
+fi
+./configure
+make && sudo make install
+make clean
