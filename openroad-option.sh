0325113055be11d6bb28b0450b0247df552388bf
Mizuki <126767133+3zki@users.noreply.github.com>
2024-12-25 23:13:15 +0900

    Update openroad-option.sh

--- openroad-option.sh
+++ openroad-option.sh
@@ -12,6 +12,39 @@ my_dir=$(dirname "$my_path")
 export SCRIPT_DIR="$my_dir"
 export openROAD_VERSION="2.0-17198-g8396d0866"
 
+
+# Update Ubuntu/Xubuntu installation
+# ----------------------------------
+# the sed is needed for xschem build
+echo ">>>> Update packages"
+sudo sed -i 's/# deb-src/deb-src/g' /etc/apt/sources.list
+sudo apt -qq update -y
+sudo apt -qq upgrade -y
+
+# Optional removal of unneeded packages to free up space, important for VirtualBox
+# --------------------------------------------------------------------------------
+#echo ">>>> Removing packages to free up space"
+# FIXME could improve this list
+#sudo apt -qq remove -y libreoffice-* pidgin* thunderbird* transmission* xfburn* \
+#	gnome-mines gnome-sudoku sgt-puzzles parole gimp*
+#sudo apt -qq autoremove -y
+
+
+# Install basic tools via apt
+# ---------------------------
+echo ">>>> Installing required packages via APT"
+# sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt -qq install -y build-essential python3-pip
+
+# unnecessary packages iic-osic will install:
+# octave octave-signal octave-communications octave-control
+# htop mc gedit vim vim-gtk3 kdiff3
+
+# setup gnome-terminal (with PATCH for Ubuntu 22 WSL2)
+# --------
+sudo apt -qq install -y gnome-terminal
+systemctl --user start gnome-terminal-server
+
 # Install/update yosys
 # --------------------
 if [ ! -d "$SRC_DIR/yosys" ]; then
@@ -34,7 +67,7 @@ make -j"$(nproc)" && sudo make install
 make clean
 
 # Install/update openroad
-# --------------------
+# -----------------------
 # if [ ! -d "$SRC_DIR/openROAD" ]; then
 #	echo ">>>> Installing qrouter"
 #	git clone https://github.com/The-OpenROAD-Project/OpenROAD.git "$SRC_DIR/openROAD"
@@ -53,13 +86,15 @@ make clean
 
 wget "https://github.com/Precision-Innovations/OpenROAD/releases/download/$openROAD_VERSION/openroad_"$openROAD_VERSION"_amd64-ubuntu-22.04.deb"
 
-# Patch: WA for openROAD package
+# Ubuntu 22 WSL patch for openROAD package
 sudo dpkg -i --force-overwrite "./openroad_"$openROAD_VERSION"_amd64-ubuntu-22.04.deb"
 # Patch end
 
 sudo apt -f -qq install -y "./openroad_"$openROAD_VERSION"_amd64-ubuntu-22.04.deb"
 rm "./openroad_"$openROAD_VERSION"_amd64-ubuntu-22.04.deb"
 
+# Create .bashrc
+# --------------
 sed -i -e '/export OPENROAD_EXE=/d' $HOME/.bashrc
 sed -i -e '/export YOSYS_EXE=/d' $HOME/.bashrc
 
